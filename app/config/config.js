3e12e97a03dd844a26604393838a61b6c1f1ff53
Bohdan Popovych <bohpopovych@gmail.com>
2023-03-22 19:34:11 -0600

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,4 @@
 export default {
-    'domain': 'https://frontend-system-monitor.herokuapp.com',
-    "api": "https://backend-system-monitor.herokuapp.com/api",
+    'domain': 'https://frontend-system-monitor.onrender.com',
+    "api": "https://backend-system-monitor.onrender.com/api",
 }
